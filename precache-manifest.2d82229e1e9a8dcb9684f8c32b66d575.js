b44850e40a8f53b0e961cec65f0dc49fdffb227d
ansh.vansh712@gmail.com <sikka008@umn.edu>
2019-07-20 15:09:32 -0500

    Updates

---
+++ precache-manifest.2d82229e1e9a8dcb9684f8c32b66d575.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e0c6dca3c3094f3273db69af5caaa63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6849dd0208390b49dd88",
+    "url": "/static/css/main.c24ea9ec.chunk.css"
+  },
+  {
+    "revision": "e509f925901104c51524",
+    "url": "/static/js/2.810b0385.chunk.js"
+  },
+  {
+    "revision": "6849dd0208390b49dd88",
+    "url": "/static/js/main.1deee0dd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);

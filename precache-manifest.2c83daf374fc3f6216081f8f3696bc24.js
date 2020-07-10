c689b20f08675736b0d986c9accc96289e086b98
Ansh Sikka <sikka008@umn.edu>
2020-07-09 21:10:11 -0500

    Updates

---
+++ precache-manifest.2c83daf374fc3f6216081f8f3696bc24.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3993416d1ced505343cc11169803ddb5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e45bfb583b0aa55a37b4",
+    "url": "./static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "d81e575fc9e445aeb5ad",
+    "url": "./static/css/main.cdcc0ca7.chunk.css"
+  },
+  {
+    "revision": "e45bfb583b0aa55a37b4",
+    "url": "./static/js/2.b485d874.chunk.js"
+  },
+  {
+    "revision": "d81e575fc9e445aeb5ad",
+    "url": "./static/js/main.cf80cfbc.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "6996b0dbf5ad63e6f206706c38ef4875",
+    "url": "./static/media/ansh.6996b0db.jpg"
+  },
+  {
+    "revision": "ccb80190ea5f5efac307f425b0450acc",
+    "url": "./static/media/resume.ccb80190.pdf"
+  }
+]);

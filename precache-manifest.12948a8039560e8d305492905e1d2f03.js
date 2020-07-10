777d8f9b97e34c70b8f394030810493d3dcf4b8f
Ansh Sikka <sikka008@umn.edu>
2020-07-09 21:50:49 -0500

    Updates

---
+++ precache-manifest.12948a8039560e8d305492905e1d2f03.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e787b0fa3591723ebbcc6b02b68f522f",
+    "url": "/portfoliowebsite/index.html"
+  },
+  {
+    "revision": "e45bfb583b0aa55a37b4",
+    "url": "/portfoliowebsite/static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "270b6ecd8b60f881a30a",
+    "url": "/portfoliowebsite/static/css/main.cdcc0ca7.chunk.css"
+  },
+  {
+    "revision": "e45bfb583b0aa55a37b4",
+    "url": "/portfoliowebsite/static/js/2.b485d874.chunk.js"
+  },
+  {
+    "revision": "270b6ecd8b60f881a30a",
+    "url": "/portfoliowebsite/static/js/main.bb6ad2db.chunk.js"
+  },
+  {
+    "revision": "9b7897b2239eb5cc8e4e",
+    "url": "/portfoliowebsite/static/js/runtime~main.ca92c413.js"
+  },
+  {
+    "revision": "6996b0dbf5ad63e6f206706c38ef4875",
+    "url": "/portfoliowebsite/static/media/ansh.6996b0db.jpg"
+  },
+  {
+    "revision": "ccb80190ea5f5efac307f425b0450acc",
+    "url": "/portfoliowebsite/static/media/resume.ccb80190.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7216add04c8e73ac07d4e5e27142afef.js"
+  "/portfoliowebsite/precache-manifest.12948a8039560e8d305492905e1d2f03.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfoliowebsite/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

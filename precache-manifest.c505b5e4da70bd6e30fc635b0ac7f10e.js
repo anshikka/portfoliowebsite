611ea44bff15c3708953a98fca87c4fd1e05f74d
Ansh Sikka <sikka008@umn.edu>
2021-08-22 18:35:26 -0500

    Updates

---
+++ precache-manifest.c505b5e4da70bd6e30fc635b0ac7f10e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "734d68e649c7dd6dc43227bf1dfe538b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dbbcb2ba9622ef6c7d5",
+    "url": "/static/css/2.10583f31.chunk.css"
+  },
+  {
+    "revision": "34f8a13d7aa7d8de83fa",
+    "url": "/static/css/main.bb57a942.chunk.css"
+  },
+  {
+    "revision": "6dbbcb2ba9622ef6c7d5",
+    "url": "/static/js/2.20b41ac1.chunk.js"
+  },
+  {
+    "revision": "34f8a13d7aa7d8de83fa",
+    "url": "/static/js/main.f640389a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6c91efc3ed9ed995a580aafb033e2fb8",
+    "url": "/static/media/ansh.6c91efc3.jpeg"
+  },
+  {
+    "revision": "2ac63345908b3b6d83b21c333b8df16b",
+    "url": "/static/media/background.2ac63345.jpg"
+  },
+  {
+    "revision": "86c18b24ae4221dd829de686246d8d42",
+    "url": "/static/media/resume.86c18b24.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.230cc16f7956a902607388f87195c039.js"
+  "/precache-manifest.c505b5e4da70bd6e30fc635b0ac7f10e.js"
 );
 
 self.addEventListener('message', (event) => {
